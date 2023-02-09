3c8acc0dbad7137c7a0d574a56f73249ae1d1257
0xyanc <yannick.chi@gmail.com>
2023-02-09 18:09:11 +0100

    add frontend

---
+++ frontend/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+const Header = () => {
+  return (
+    <Flex h="5vh" p="2rem" justifyContent="space-between" alignItems="center">
+      <Text as="b">Snowfall Staking App</Text>
+      <ConnectButton />
+    </Flex>
+  );
+};
+
+export default Header;
---
+++ frontend/pages/_app.js
@@ -0,0 +1,45 @@
+import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { hardhat } from 'wagmi/chains';
+// import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { ChakraProvider } from '@chakra-ui/react';
+import Layout from '@/components/Layout/Layout';
+const { chains, provider } = configureChains(
+  [hardhat],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Snowfall Staking App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </RainbowKitProvider>\
+    </WagmiConfig>
+  )
+}
